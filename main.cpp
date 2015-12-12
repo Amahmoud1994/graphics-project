1b2c308acc493ff2f1cdff24a79cc0af73bf7f28
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-12 18:12:38 +0200

    Modifies main func

--- main.cpp
+++ main.cpp
@@ -1,108 +1,33 @@
 #include <GL/glut.h>
-#include <sstream>
-#include "math.h"
+#include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <stddef.h>
+#include "math.h"
 
-#include "utils.h"
-#include "terrain.h"
 #include "car.h"
-#include "brick.h"
-#include "cone.h"
 
-Terrain *terrain = new Terrain();
+using namespace std;
+void timer(int);
+void keyboardHandler(unsigned char, int, int);
+void drawAxes();
+void mouseRotation();
 
-Car* car = new Car();
+const int FPS = 33;
+unsigned char Buttons[3] = { 0 };
+int lastx = 0;
+int lasty = 0;
+float rotx = 0;
+float roty = 0;
+float rotz = 0;
 
-vector <Brick> bricks;
-vector <Cone> cones;
+Car* car = new Car(0);
 
-int TCNT = 0; // Time counter
-
-void Switch_Key(int, int, int);
-void drawTextureCube(void);
-
-void render(void) {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Camera
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  if(!gameOver)
-  {
-  glRotatef(rotx, 1, 0, 0);
-  glRotatef(roty, 0, 1, 0);
-  glRotatef(rotz, 0, 0, 1);
-
-  //Draw the Texturing Cube
-  drawTextureCube();
-
-  // Axes
-  drawAxes();
-
-  // Terrain
-  terrain->draw();
-
-  // Car
-  car->draw();
-
-  // Bricks
-  if (!bricks.empty()) {
-    for(size_t i = 0; i < bricks.size(); i++) {
-      bricks[i].draw();
-    }
-  }
-
-  // Cones
-  if (!cones.empty()) {
-    for(size_t i = 0; i < cones.size(); i++) {
-      cones[i].draw();
-    }
-  }
-}else
-{
-  drawBitmapText("You Lost Try Again Later", 7, 10,2);
-}
-  glutSwapBuffers();
-}
-
-void update() {
-  terrain->update();
-
-  if (!bricks.empty()) {
-    for(size_t i = 0; i < bricks.size(); i++) {
-      bricks[i].update();
-    }
-  }
-
-  if (!cones.empty()) {
-    for(size_t i = 0; i < cones.size(); i++) {
-      cones[i].update();
-    }
-  }
-}
-
-void timer(int t) {
-
-  update();
-
-  if(TCNT == 80) {
-    int brickX = generateRandom(-1, -9);
-    int brickZ = generateRandom(0, 3);
-    Brick *brick = new Brick(brickX, brickZ);
-    bricks.push_back(*brick);
-
-    int coneX = generateRandom(-1, -9);
-    int coneZ = generateRandom(0, 3);
-    Cone *cone = new Cone(coneX, coneZ);
-    cones.push_back(*cone);
-
-    TCNT = 0;
-  }
-
-  TCNT++;
-
-  glutPostRedisplay();
-  glutTimerFunc(FPS, timer, 0);
+void mouseRotation(){
+        glRotatef(rotx, 1, 0, 0);
+        glRotatef(roty, 0, 1, 0);
+        glRotatef(rotz, 0, 0, 1);
 }
 
 void motion(int x, int y)
@@ -113,23 +38,12 @@ void motion(int x, int y)
 	lasty = y;
 	if (Buttons[0])
 	{
-		rotx += (float) 0.5f * diffy;
-		roty += (float) 0.5f * diffx;
+		rotx += (float) 0.2f * diffy;
+		roty += (float) 0.2f * diffx;
 	}
 }
 
-void drawTextureCube()
-{
-  glPushMatrix(); // The cubic body
-  glColor3f(0,0,0);
-  glutWireCube(50);
-  glPopMatrix();
-}
-
-void keyboardHandler(unsigned char key, int x, int y) {
-}
-
-void Mouse(int b, int s, int x, int y) {
+void mouse(int b, int s, int x, int y) {
   lastx = x;
 	lasty = y;
 
@@ -148,51 +62,84 @@ void Mouse(int b, int s, int x, int y) {
 	}
 }
 
-void Switch_Key(int key, int x, int y)
-{
-  switch (key) {
-    case GLUT_KEY_UP    :  if(car->xCoordinate > 0)car->xCoordinate-=0.1;break;
-    case GLUT_KEY_DOWN  :  if(car->xCoordinate < terrain->pathLength)car->xCoordinate+=0.1;break;
-    case GLUT_KEY_RIGHT :  if(car->zCoordinate > 0)car->zCoordinate-=0.1;break;
-    default             :  if(car->zCoordinate < terrain->pathWidth)car->zCoordinate+=0.1;break;
-  }
+void render(void) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glPushMatrix();
+        mouseRotation();
+        car->draw();
+        drawAxes();
+        glPopMatrix();
+        glutSwapBuffers();
 }
 
-void decScore()
-{
-  gameScore--;
-  if(gameScore ==0)
-    gameOver=true;
+void timer(int t) {
 
+        glutPostRedisplay();
+        glutTimerFunc(FPS, timer, 0);
+}
+
+void keyboardHandler(unsigned char key, int x, int y){
+
+}
+
+void drawAxes(void){
+        glPushMatrix();
+
+        glLineWidth(2.0);
+
+        glBegin(GL_LINES);
+        glColor3f(1,0,0); // x axis is red.
+        glVertex3f(0.0f,0.0f,0.0f);
+        glVertex3f(500.0f,0.0f,0.0f);
+        glColor3f(0,1,0); // y axis is green.
+        glVertex3f(0.0f,0.0f,0.0f);
+        glVertex3f(0.0f,500.0f,0.0f);
+        glColor3f(0,0,1); // z axis is blue.
+        glVertex3f(0.0f,0.0f,0.0f);
+        glVertex3f(0.0f,0.0f,500.0f);
+        glEnd();
+
+        glPopMatrix ();
+}
+
+void initLighting(){
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_NORMALIZE);
+        glEnable(GL_COLOR_MATERIAL);
+        glEnable(GL_SMOOTH);
+        glShadeModel(GL_SMOOTH);
 }
 
 int main(int argc, char** argv) {
-  glutInit(&argc, argv);
+        glutInit(&argc, argv);
 
-  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-  glutInitWindowPosition(150, 150);
+        glutInitWindowSize(800, 600);
+        glutInitWindowPosition(150, 150);
 
-  glutCreateWindow("Race the Moon");
-  glutDisplayFunc(render);
-  glutTimerFunc(0, timer, 0);
-  glutMotionFunc(motion);
-  glutKeyboardFunc(keyboardHandler);
-  glutMouseFunc(Mouse);
-  glutSpecialFunc(Switch_Key);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+        glutCreateWindow("Car Crash");
+        glutDisplayFunc(render);
+        glutTimerFunc(0, timer, 0);
+        glutMotionFunc(motion);
+        glutMouseFunc(mouse);
+        glutKeyboardFunc(keyboardHandler);
 
-  glEnable(GL_DEPTH_TEST);
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
-  initLighting();
+        glEnable(GL_DEPTH_TEST);
 
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(45.0f, WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 300.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45.0f, 800 / 600, 0.1f, 300.0f);
 
-  gluLookAt(7.0, 3.0, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(0.0f, 5.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
-  glutMainLoop();
+        initLighting();
 
-  return 0;
+        glutMainLoop();
+
+        return 0;
 }

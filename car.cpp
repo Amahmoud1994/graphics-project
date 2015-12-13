a32d66b62031eab4269c54a004e0952772006914
Abdelrahman <a.mahmoud1994@gmail.com>
2015-12-13 18:49:23 +0200

    Fixing the Car body

--- car.cpp
+++ car.cpp
@@ -19,12 +19,12 @@ void Car::draw(){
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(0.3f,-0.2f,0.6f);
+  glTranslatef(0.3f,-0.2f,0.55f);
   drawWheel();
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(0.6f,-0.2f,0.6f);
+  glTranslatef(0.6f,-0.2f,0.55f);
   drawWheel();
   glPopMatrix();
   // End Wheels
@@ -32,21 +32,26 @@ void Car::draw(){
   //Body
   glPushMatrix(); // The cubic body
   glColor3f((92/255.0),(51/255.0),(23/255.0));
-  glTranslatef(this->xCoordinate+0.5,this->yCoordinate,this->zCoordinate+0.5);
-  glutSolidCube(0.4);
+  glTranslatef(this->xCoordinate+0.5,this->yCoordinate+0.05,this->zCoordinate+0.5);
+  glutSolidCube(0.44);
   glPopMatrix();
 
   //Cabout
+
+
   glPushMatrix(); // The cubic body
   glColor3f((92/255.0),(51/255.0),(23/255.0));
-  glTranslatef(this->xCoordinate+0.25,this->yCoordinate-0.05,this->zCoordinate+0.4);
+  glTranslatef(this->xCoordinate+0.4,this->yCoordinate-0.05,this->zCoordinate+0.2);
+  glScalef(1.2,1,1);
   glutSolidCube(0.2);
   glPopMatrix();
   glPushMatrix(); // The cubic body
   glColor3f((92/255.0),(51/255.0),(23/255.0));
-  glTranslatef(this->xCoordinate+0.25,this->yCoordinate-0.05,this->zCoordinate+0.6);
+  glTranslatef(this->xCoordinate+0.6,this->yCoordinate-0.05,this->zCoordinate+0.2);
+  glScalef(1.2,1,1);
   glutSolidCube(0.2);
   glPopMatrix();
+
   glPopMatrix();
 }
 
@@ -59,7 +64,6 @@ void Car::drawWheel(){
   glPushMatrix();
   glColor3f(0,0,0);
   glTranslatef(this->xCoordinate, this->yCoordinate, this->zCoordinate+0.1);
-//  glRotatef(180, 1,0,0);
   glRotatef(angle,0,1,0);
   gluDisk(q, 0.0f, 0.1, 15, 1);
   glPopMatrix();
@@ -74,7 +78,6 @@ void Car::drawWheel(){
   glPushMatrix();
   glColor3f(0,0,0);
   glTranslatef(this->xCoordinate, this->yCoordinate, this->zCoordinate+0.1);
-  //glRotatef(180, 1,0,0);
   glRotatef(angle,0,1,0);
   gluDisk(q, 0.0f, 0.1, 15, 1);
   glPopMatrix();
